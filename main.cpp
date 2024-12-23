e8097116304eee7c74f3ed0bd9a7dff51fa04061
Ivxn7 <ivxnnn11@gmail.com>
2024-12-23 17:21:30 +0800

    Solved

--- main.cpp
+++ main.cpp
@@ -1,65 +1,14 @@
 #include "functions.h"
 int check_names(string *names, int i);
 void enter_name(string *names, int num_of_participants);
-// int main()
-// {
-//     srand(time(0));
-//     int pax, choice, numprize;
-//     int qtyprize[100];
-//     char cont;
-//     string name[100], prize[100];
 
-//     do
-//     {
-//         cout << "Pleasse choose the mode...\n";
-//         cout << "1)Single winner mode" << endl;
-//         cout << "2)Three winner mode" << endl;
-//         cout << "3)Custome mode" << endl;
-//         cout << "Enter choice:";
-//         cin >> choice;
 
-//         if (choice == 1)
-//         {
 
-//             singlewinner(name, prize, pax, numprize, qtyprize);
-//         }
-//         else if (choice == 2)
-//         {
 
-//             triplewinner(name, prize, pax, numprize, qtyprize);
-//         }
-//         else if (choice == 3)
-//         {
-//             singlewinner(name, prize, pax, numprize, qtyprize);
-//         }
-//         else
-//         {
-//             cout << "Invalid input!!\n\n";
-//         }
-//         cout << "\nDo you want to continue(y/n):";
-//         cin >> cont;
-//     } while (cont == 'y' || cont == 'Y');
 
-//     return 0;
-// }
-// void swap(string &a, string &b)
-// {
-//     string tmp;
-//     tmp = a;
-//     a = b;
-//     b = tmp;
-// }
 void swap_int(int &a, int &b)
 {
     int tmp;
-    tmp = a;
-    a = b;
-    b = tmp;
-}
-
-void swap_ptr(string a, string b)
-{
-    string tmp;
     tmp = a;
     a = b;
     b = tmp;
@@ -96,48 +45,66 @@ void generate_prize(string *prize, int prize_num, int ori_draws, int *win)
         num[i] = num[r];
         win[(draws - 1) - i] = num[r];
         num[r] = tmp;
-        // for( int j=0; j<10; j++ ) //check util for algo array
-        //     cout << num[j]<< " ";
-        // cout <<endl;
+        
     }
-    // cout << "The winning draw: ";   //check util for win array
-    // for( int j=0; j<3; j++ )
-    //     cout << win[j]<< " ";
-    // cout <<endl;
+    cout << "The winning draw: ";   //check util for win array
+    for( int j=0; j<9; j++ )
+        cout << win[j]<< " ";
+    cout <<endl;
 }
-void start_draw(string *names, int p_num, string *prize, int draws, int *winning_draw, int cds, int *d_num)
+int start_draw(string *names, int p_num, string *prize, int draws, int *winning_draw, int cds, int *d_num, int prize_num)
 {
+    int available;
     srand(time(0));
     int result;
-    // string participants[2] = {"ivan", "yo"};
+    int check_draw;
     for (int i = p_num - 1; i > 0; i--)
     {
         int r = rand() % i;
-        swap_ptr(names[i], names[r]);   //problem here at the fisher Yates algo
-    }
-    cout << names[0];
-    // cout << *names;
-    
-    // cout << participants[0];
-    // cout <<
-//     for (int i = 0; i < p_num; i++)
-//     {
-//         cout << "Participant: " << participants[i] << "You can draw now!"<<endl;
-//         for (int j = draws - cds; j > draws - (draws - 1); j--)
-//         {
-//             int r_draws = rand() % j;
-//             result = d_num[r_draws];
-//             swap_int(d_num[j], d_num[r_draws]);
-//         }
+        swap(names[i], names[r]);
         
-//         cout << "Participant "<<participants[i]<<" You have drawn a "<<result<<"!"<<endl;
-//         auto it = find(winning_draw, winning_draw+p_num, result);
-//         if (it != winning_draw + p_num)
-//         {  
-//             int idx = it - winning_draw;
-//             cout << "You have won a "<<prize[idx]<<endl;
-//         }
-//     }
+    }
+    int current_draw = cds;
+    cout << winning_draw[0]<<" and "<<winning_draw[1]<<"\n\n";
+
+    for (int i = 0; i < p_num; i++) //loop each turns of each participants
+    {   
+        available = draws - current_draw - 1;
+        if(int p_length == sizeof){
+            
+        }
+        elif (available == 0){
+            cout << "Participant "<<names[i]<<" You have drawn a "<<d_num[0]<<"!"<<endl;
+            auto it = find(winning_draw, winning_draw+prize_num, d_num[0]);
+            if (it != winning_draw + prize_num)
+            {  
+                int idx = it - winning_draw;
+                cout << "You have won a "<<prize[idx]<<endl;
+                check_draw += 1;
+            }
+            cout << "----------------------------------------------------------"<<endl;
+        }
+        else
+        {
+            int r_draws = rand() % available;
+            int result = d_num[r_draws];
+            swap(d_num[available], d_num[r_draws]);
+            current_draw += 1;
+            cout << "Participant "<<names[i]<<" You have drawn a "<<result<<"!"<<endl;
+            auto it = find(winning_draw, winning_draw+prize_num, result);
+            if (it != winning_draw + prize_num)
+            {  
+                int idx = it - winning_draw;
+                cout << "You have won a "<<prize[idx]<<endl;
+                check_draw += 1;
+            }
+            // for( int j=0; j<10; j++ ) //check util for algo array
+            //     cout << d_num[j]<< " ";
+            // cout <<endl;
+            cout << "----------------------------------------------------------"<<endl;
+        }
+    }
+    
 }
 
 int main()
@@ -147,7 +114,8 @@ int main()
     int num_of_participants;
     char cont;
     int current_draw_size = 0;
-    int draw_num[draws];
+    int *d_num = new int[draws];
+    int check_draw;
 
     do
     {
@@ -158,7 +126,7 @@ int main()
             cin.clear();
             cin.ignore(numeric_limits<streamsize>::max(), '\n');
         }
-
+        
         string *names = new string[num_of_participants];
         cout << "Input the names of participants" << endl;
         enter_name(names, num_of_participants);
@@ -180,26 +148,28 @@ int main()
             cin.ignore(numeric_limits<streamsize>::max(), '\n');
         }
 
+        for (int j = 0; j < draws; j++) //if d_num is initialized as a normal array, num_of participants value changes to 4 no matter what.
+            d_num[j] = j + 1;
         string *prize = new string[prize_num];
         int *winning_draw = new int[prize_num];
         generate_prize(prize, prize_num, draws, winning_draw);
         
-        for (int i = 0; i < draws; i++)
-            draw_num[i] = i + 1;
-        for (int i = 0; i < 10; i++)
-        {
-            start_draw(names, num_of_participants, prize, draws, winning_draw, current_draw_size, draw_num);
-            current_draw_size += 3;
+        for (int i = 0; i < 5; i++)
+        {   
+            if (current_draw_size != 0 && draws % current_draw_size == draws){
+                cout <<"Fuck otter";
+                break;
+            }
+            cout << "======================================== Round "<<i + 1<<" Starts========================================"<<endl;
+            check_draw += start_draw(names, num_of_participants, prize, draws, winning_draw, current_draw_size, d_num, prize_num);
+            current_draw_size += num_of_participants;
         }
 
         cout << "\nDo you want to continue(y/n):";
         cin >> cont;
     } while (cont == 'y' || cont == 'Y');
+    return 0;
 }
-
-// for (int i=0; i<num_of_participants;i++){ //debug line
-//     cout << names[i] << endl;
-// }
 
 int check_names(string *names, int i)
 {

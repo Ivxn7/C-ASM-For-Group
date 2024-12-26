064c1f64fd042a853cbb6b39f7af8709a6f7d233
Ivxn7 <ivxnnn11@gmail.com>
2024-12-26 18:57:03 +0800

    Included comments explaining the code

--- main.cpp
+++ main.cpp
@@ -2,10 +2,10 @@
 
 int main()
 {
-    int draws;
-    int prize_num;
-    int num_of_participants;
-    char cont;
+    int draws;                      //number of draws
+    int prize_num;                  //number of prize
+    int num_of_participants;        //number of participants
+    char cont;                      //do while
     int current_draw_size;
     int check_draw;
 
@@ -13,13 +13,13 @@ int main()
     {   
         current_draw_size = 0;
         check_draw = 0;
-        cout << "======================="<<endl;
+        cout << "======================="<<endl;    //initialise lucky draw (from line 16 to line 48.)
         loading_ani("Initialising Lucky Draw");
         cout << "======================="<<endl;
         cout << "Calibration starts: ";
         cout << "Please enter:"<<endl;
         cout << "Number of participants: ";
-        while (!(cin >> num_of_participants) || num_of_participants < 2)
+        while (!(cin >> num_of_participants) || num_of_participants < 2) //participants cannot be less than 2.
         {
             cout << "Invalid input. Please have at least 2 participants: ";
             cin.clear();
@@ -28,11 +28,11 @@ int main()
         
         string *names = new string[num_of_participants];
         cout << "\nInput the names of participants" << endl;
-        enter_name(names, num_of_participants);
+        enter_name(names, num_of_participants); //check if there are duplicate names
         
 
         cout << "=======================\nNumber of lucky draws:";
-        while (!(cin >> draws) || draws < 10 || draws > 100)
+        while (!(cin >> draws) || draws < 10 || draws > 100) // number of draws cannot be less than 10 and more than 100
         {
             cout << "Invalid input. Please enter a draw number between 10 and 100: ";
             cin.clear();
@@ -40,31 +40,31 @@ int main()
         }
 
         cout << "=======================\nNumber of prize:";
-        while (!(cin >> prize_num) || prize_num > 10 || draws < 0)
+        while (!(cin >> prize_num) || prize_num > 10 || prize_num < 1) //prize cannot be more than 10 and less than 1.
         {
             cout << "Invalid input. Please set the number of prize less or equal than 10:";
             cin.clear();
             cin.ignore(numeric_limits<streamsize>::max(), '\n');
         }
 
-        int *d_num = new int[draws];
-        for (int j = 0; j < draws; j++) 
+        int *d_num = new int[draws];   
+        for (int j = 0; j < draws; j++)  //this for loop initialise an array to store a number of elements equivalent to the draws.
             d_num[j] = j + 1;
         string *prize = new string[prize_num];
         int *winning_draw = new int[prize_num];
-        generate_prize(prize, prize_num, draws, winning_draw);
+        generate_prize(prize, prize_num, draws, winning_draw); //generate winning draws and the prize for each winning draws.
         
         for (int i = 0; i < 10; i++)
-        {   if (check_draw == prize_num){
+        {   if (check_draw == prize_num){           //check_draw will be incremented if the prize is drawn. If check_draw is equal to prize_num, which is the total number of prize, the lucky draw will end.
                 cout << "The lucky draw has ended";
                 break;
             }
-            else if (current_draw_size != 0 && draws % current_draw_size == draws){
+            else if (current_draw_size != 0 && draws % current_draw_size == draws){ //check if the amount of drawn numbers is equal to draws.
                 cout <<"The lucky draw has ended";
                 break;
             }
             else{
-                cout << "========================================Round "<<i + 1<<" Starts========================================"<<endl;
+                cout << "========================================Round "<<i + 1<<"  Starts========================================"<<endl; //starts each round for the lucky draw.
                 loading_ani("The participants are drawing");
                 check_draw = start_draw(names, num_of_participants, prize, draws, winning_draw, current_draw_size, d_num, prize_num, check_draw);
                 current_draw_size += num_of_participants;
--- myfunctions.cpp
+++ myfunctions.cpp
@@ -1,6 +1,6 @@
 #include "functions.h"
 
-string randomizer(vector<string> items_to_be_randomize)
+string randomizer(vector<string> items_to_be_randomize)  //function to pick a prize randomly for each winning draw.
 {
     srand(time(0));
     string random;
@@ -10,21 +10,21 @@ string randomizer(vector<string> items_to_be_randomize)
 
 void generate_prize(string *prize, int prize_num, int ori_draws, int *win)
 {
-    vector<string> p_name = {"luxury vacation package for two", "a high-end smartphone with accessories", "a premium smartwatch", "$200 gift card", "gourmet gift basket with chocolates and fine wines", "smart home device bundle", "one-year gym membership with fitness gear", "luxurious spa day", "VIP passes to a concert or sports event", "entertainment vouchers"};
+    vector<string> p_name = {"luxury vacation package for two", "a high-end smartphone with accessories", "a premium smartwatch", "$200 gift card", "gourmet gift basket with chocolates and fine wines", "smart home device bundle", "one-year gym membership with fitness gear", "luxurious spa day", "VIP passes to a concert or sports event", "entertainment vouchers"}; 
     vector<string> tmp = p_name;
     int draws = ori_draws;
     int num[draws];
-    for (int i = 0; i < prize_num; i++)
+    for (int i = 0; i < prize_num; i++) //based on the number of prize, assign the prizes into the prize array. Chosen prize will be erased.
     {
         string get_prize = randomizer(tmp);
         prize[i] = get_prize;
         tmp.erase(find(tmp.begin(), tmp.end(), get_prize));
     }
-    for (int i = 0; i < draws; i++)
+    for (int i = 0; i < draws; i++) //generate a temporary array with all the draw numbers up to the total draws.
     {
         num[i] = i + 1;
     }
-    for (int i = draws - 1; i > draws - prize_num - 1; i--)
+    for (int i = draws - 1; i > draws - prize_num - 1; i--) //randomly select the winning draws from the temporary array. Selected numbers will be not be picked again.
     {
         srand(time(NULL));
         int r = rand() % i;
@@ -34,10 +34,10 @@ void generate_prize(string *prize, int prize_num, int ori_draws, int *win)
         num[r] = tmp;
         
     }
-    cout << "The winning draw: "; //needs to be deleted
-    for( int j=0; j<prize_num; j++ )
-        cout << win[j]<< " ";
-    cout <<endl;
+    // cout << "The winning draw: "; //to check the winning draw
+    // for( int j=0; j<prize_num; j++ )
+    //     cout << win[j]<< " ";
+    // cout <<endl;
 }
 
 
@@ -48,7 +48,7 @@ int start_draw(string *names, int p_num, string *prize, int draws, int *winning_
 
     srand(time(0));
     cout << "=============================================================================================="<<endl;
-    for (int i = p_num - 1; i > 0; i--)
+    for (int i = p_num - 1; i > 0; i--) //randomise the order of participants each round
     {
         int r = rand() % i;
         swap(names[i], names[r]);
@@ -56,16 +56,16 @@ int start_draw(string *names, int p_num, string *prize, int draws, int *winning_
     }
     int current_draw = cds;
 
-    for (int i = 0; i < p_num; i++)
+    for (int i = 0; i < p_num; i++) //loop n amount of times, n = number of participants
     {   
-        available = draws - current_draw - 1;
-        if(check_draw == prize_num){
+        available = draws - current_draw - 1; //the amount of draws left.
+        if(check_draw == prize_num){  //check if the amount of drawn price is equal to the number of prize. If this statement is true, lucky draw ends.
             return check_draw;
         }
-        else if (available == 0){
+        else if (available == 0){  //If there's only 1 draw left, automatically gives the draw number to the corresponding participant.
             cout << "Participant "<<names[i]<<" You have drawn a "<<d_num[0]<<"!"<<endl;
             auto it = find(winning_draw, winning_draw+prize_num, d_num[0]);
-            if (it != winning_draw + prize_num)
+            if (it != winning_draw + prize_num) //check if the number is a winning draw
             {  
                 int idx = it - winning_draw;
                 cout << "You have won a "<<prize[idx]<<endl;
@@ -75,12 +75,14 @@ int start_draw(string *names, int p_num, string *prize, int draws, int *winning_
         }
         else
         {
-            int r_draws = rand() % available;
+
+            //r_draws is assigned with a randomly selected index in the range of 1 and the number of available draws. Then, the algo will find the number in the d_num array based on the given index. The selected number will then be swapped with the last number in the array. Current_draw will be incremented by 1 and available will be decremented by 1 simultaneously. In this way, the indexes of the numbers that are less than available will not be picked anymore.//
+            int r_draws = rand() % available; 
             int result = d_num[r_draws];
             swap(d_num[available], d_num[r_draws]);
             current_draw += 1;
             cout << "Participant "<<names[i]<<" You have drawn a "<<result<<"!"<<endl;
-            auto it = find(winning_draw, winning_draw+prize_num, result);
+            auto it = find(winning_draw, winning_draw+prize_num, result); //check if the number is a winning draw
             if (it != winning_draw + prize_num)
             {  
                 int idx = it - winning_draw;
@@ -93,7 +95,7 @@ int start_draw(string *names, int p_num, string *prize, int draws, int *winning_
     return check_draw;
 }
 
-int check_names(string *names, int i)
+int check_names(string *names, int i) //check if the user input name has already exist
 {
     auto it = find(names, names + i, names[i]);
     if (it != names + i)
@@ -105,7 +107,7 @@ int check_names(string *names, int i)
     return 1;
 }
 
-void enter_name(string *names, int num_of_participants)
+void enter_name(string *names, int num_of_participants) //prompt user to input name
 {
 
     for (int i = 0; i < num_of_participants; i++)
@@ -125,7 +127,7 @@ void enter_name(string *names, int num_of_participants)
     return;
 }
 
-void loading_ani(string phrase)
+void loading_ani(string phrase) //a small loading terminal output for aesthetic purposes.
 {
     cout << phrase;
     
